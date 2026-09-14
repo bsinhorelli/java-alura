368b12289170d7764e275a020d115e1d8c9e5bbf
Bruno Sinhorelli <b.sinhorelli@gmail.com>
2026-09-14 18:58:10 -0300

    feat: adicionar classificacao de estrelas

--- src/Main.java
+++ src/Main.java
@@ -19,5 +19,8 @@ public class Main {
                 Ano de lançamento:""" + anoDeLancamento;
         System.out.println(sinopse);
 
+        int classificacao = (int) (media / 2);
+        System.out.println(classificacao);
+
     }
 }
